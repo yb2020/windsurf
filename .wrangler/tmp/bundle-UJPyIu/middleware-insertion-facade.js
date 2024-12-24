b3043865c75b9e707023061dff33f92343da5909
yibing <yibing@idea.edu.cn>
2024-12-24 19:23:11 +0800

    Fix deployment configuration for Pages and Workers

---
+++ .wrangler/tmp/bundle-UJPyIu/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "/Users/yibing/windsurf/workers/index.ts";
+				import * as __MIDDLEWARE_0__ from "/Users/yibing/windsurf/node_modules/.pnpm/wrangler@3.99.0_@cloudflare+workers-types@4.20241218.0/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "/Users/yibing/windsurf/node_modules/.pnpm/wrangler@3.99.0_@cloudflare+workers-types@4.20241218.0/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+
+				export * from "/Users/yibing/windsurf/workers/index.ts";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
--- .wrangler/tmp/dev-uuLy0c/index.js
+++ .wrangler/tmp/dev-uuLy0c/index.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// .wrangler/tmp/bundle-P7byH5/checked-fetch.js
+// .wrangler/tmp/bundle-UJPyIu/checked-fetch.js
 var urls = /* @__PURE__ */ new Set();
 function checkURL(request, init) {
   const url = request instanceof URL ? request : new URL(
@@ -2185,7 +2185,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-P7byH5/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-UJPyIu/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -2217,7 +2217,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-P7byH5/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-UJPyIu/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
